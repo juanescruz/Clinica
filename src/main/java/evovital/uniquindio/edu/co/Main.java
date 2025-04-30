3a528a4308ddf0e352afa8808ec7221e1fa8a54a
Juan Esteban Cruz <juane.cruzp@uqvirtual.edu.co>
2025-04-30 08:08:35 -0500

    Prueba

--- src/main/java/evovital/uniquindio/edu/co/Main.java
+++ src/main/java/evovital/uniquindio/edu/co/Main.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
+    //Comentario para probar 1
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
